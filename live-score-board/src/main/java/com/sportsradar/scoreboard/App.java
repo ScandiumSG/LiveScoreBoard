a26cfc29d1cc62f00f64092e1ab145a0cfafd1df
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-10-01 21:10:59 +0200

    first commit

---
+++ live-score-board/src/main/java/com/sportsradar/scoreboard/App.java
@@ -0,0 +1,10 @@
+package com.sportsradar.scoreboard;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
