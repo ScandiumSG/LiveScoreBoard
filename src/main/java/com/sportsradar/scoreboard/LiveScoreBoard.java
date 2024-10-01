595e11263681aa3fcac5149f15b5b74099955738
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-10-01 21:49:44 +0200

    ScoreBoard - Added scaffolding methods for LiveScoreBoard and Match classes

---
+++ src/main/java/com/sportsradar/scoreboard/LiveScoreBoard.java
@@ -0,0 +1,25 @@
+package com.sportsradar.scoreboard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LiveScoreBoard {
+    private HashMap<String, Match> LiveMatches = new HashMap<String, Match>();
+
+    public LiveScoreBoard() {
+
+    }
+
+    public void InitiateMatch(String HomeTeam, String AwayTeam) {
+        //TODO: Implement functionality
+    }
+
+    public ArrayList<Match> GetScores() {
+        //TODO: Implement functionality
+        return null;
+    }
+
+    public void CloseMatch() {
+
+    }
+}
---
+++ src/main/java/com/sportsradar/scoreboard/Match.java
@@ -0,0 +1,27 @@
+package com.sportsradar.scoreboard;
+
+import java.util.ArrayList;
+
+public class Match {
+
+    private String HomeTeamName;
+    private String AwayTeamName;
+
+    private Integer HomeScore = 0;
+    private Integer AwayScore = 0;
+
+    public Match(String HomeTeam, String AwayTeam) {
+        HomeTeamName = HomeTeam;
+        AwayTeamName = AwayTeam;
+    }
+
+    public void UpdateScore(Integer HomeTeamScore, Integer AwayTeamScore) {
+        //TODO: Implement this functionality
+    }
+
+    public ArrayList<Integer> GetMatchScore() {
+        //TODO: Implement this functionality
+        return null;
+    }
+
+}
